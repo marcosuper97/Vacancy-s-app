1767d8e568742fcc700a1f7721e23b17abc9025b
DmitryAkh <165409513+DmitryAkh@users.noreply.github.com>
2025-07-06 19:32:54 +0300

    Переименовал функцию isInternetAvailable для понимания

--- app/src/main/java/ru/practicum/android/diploma/util/ConnectChecker.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/ConnectChecker.kt
@@ -1,4 +1,5 @@
 package ru.practicum.android.diploma.util
+
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
@@ -11,7 +12,7 @@ suspend fun isInternetAvailable(context: Context): Boolean {
         connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
     return capabilities?.run {
         hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
-            hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
-            hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+                hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
     } ?: false
 }
