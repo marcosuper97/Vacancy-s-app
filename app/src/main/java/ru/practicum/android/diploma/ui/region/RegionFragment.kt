d5028a6b846d7a97c24c6bf169e90b77ea048bf3
KotlinPunk <mak351218@gmail.com>
2025-07-06 21:14:59 +0500

    Изменения №3: Немного вёрстки, перенос фрагментов по пакетам.

--- app/src/main/java/ru/practicum/android/diploma/ui/region/RegionFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/region/RegionFragment.kt
@@ -1,10 +1,10 @@
-package ru.practicum.android.diploma.presentation.fragment
+package ru.practicum.android.diploma.ui.region
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.databinding.FragmentRegionBinding
 
 class RegionFragment : Fragment() {
--- app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
@@ -1,10 +1,10 @@
-package ru.practicum.android.diploma.presentation.fragment
+package ru.practicum.android.diploma.ui.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
