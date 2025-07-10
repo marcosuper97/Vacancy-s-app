a594a1a5233a1d4e5834d42153138d31e11612b1
DmitryAkh <165409513+DmitryAkh@users.noreply.github.com>
2025-07-10 16:56:45 +0300

    Реализовал интерактор

--- app/src/main/java/ru/practicum/android/diploma/data/db/Converter.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/Converter.kt
@@ -1,34 +1,55 @@
 package ru.practicum.android.diploma.data.db
 
-import ru.practicum.android.diploma.data.dto.AddressDto
-import ru.practicum.android.diploma.data.dto.Area
-import ru.practicum.android.diploma.data.dto.EmployerDto
-import ru.practicum.android.diploma.data.dto.EmploymentFormDto
-import ru.practicum.android.diploma.data.dto.ExperienceDto
-import ru.practicum.android.diploma.data.dto.LogoUrlDto
-import ru.practicum.android.diploma.data.dto.SalaryRangeDto
-import ru.practicum.android.diploma.data.dto.VacancyDto
-import ru.practicum.android.diploma.data.dto.WorkFormatDto
+import ru.practicum.android.diploma.domain.entity.Vacancy
 
 object Converter {
-    fun map(vacancy: VacancyEntity): VacancyDto {
-        return VacancyDto(
+
+    fun map(vacancyEntity: VacancyEntity): Vacancy {
+        return Vacancy(
+            id = vacancyEntity.id,
+            url = vacancyEntity.url,
+            name = vacancyEntity.name,
+            currency = vacancyEntity.currency,
+            salaryFrom = vacancyEntity.salaryFrom,
+            salaryTo = vacancyEntity.salaryTo,
+            employer = vacancyEntity.employer,
+            employerLogo = vacancyEntity.employerLogo,
+            area = vacancyEntity.area,
+            experience = vacancyEntity.experience,
+            workFormat = vacancyEntity.workFormat,
+            employmentForm = vacancyEntity.employmentForm,
+            description = vacancyEntity.description,
+            city = vacancyEntity.city,
+            street = vacancyEntity.street,
+            building = vacancyEntity.building,
+            addressDescription = vacancyEntity.addressDescription,
+            keySkills = vacancyEntity.keySkills,
+            additionTime = vacancyEntity.additionTime,
+            isFavorite = true
+        )
+    }
+
+    fun map (vacancy: Vacancy): VacancyEntity {
+        return VacancyEntity(
             id = vacancy.id,
+            url = vacancy.url,
             name = vacancy.name,
-            employer = EmployerDto(logoUrls = LogoUrlDto(size90 = vacancy.employerLogo), name = vacancy.name),
-            area = Area(name = vacancy.area),
-            salaryRange = SalaryRangeDto(
-                currency = vacancy.currency,
-                from = vacancy.salaryFrom,
-                to = vacancy.salaryTo,
-                gross = null
-            ),
-            workFormat = WorkFormatDto(name = vacancy.workFormat),
-            experience = ExperienceDto(name = vacancy.experience),
-            employmentFrom = EmploymentFormDto(name = vacancy.employmentForm),
+            currency = vacancy.currency,
+            salaryFrom = vacancy.salaryFrom,
+            salaryTo = vacancy.salaryTo,
+            employer = vacancy.employer,
+            employerLogo = vacancy.employerLogo,
+            area = vacancy.area,
+            experience = vacancy.experience,
+            workFormat = vacancy.workFormat,
+            employmentForm = vacancy.employmentForm,
             description = vacancy.description,
-            address = AddressDto(city = vacancy.city, street = vacancy.street, building = vacancy.building, description = vacancy.addressDescription),
-            keySkills = vacancy.keySkills
+            city = vacancy.city,
+            street = vacancy.street,
+            building = vacancy.building,
+            addressDescription = vacancy.addressDescription,
+            keySkills = vacancy.keySkills,
+            additionTime = System.currentTimeMillis()
         )
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
@@ -1,6 +1,12 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.domain.interactors.FavoritesInteractor
+import ru.practicum.android.diploma.domain.interactors.FavoritesInteractorImpl
 
 val interactorModule = module {
+
+    single<FavoritesInteractor> {
+        FavoritesInteractorImpl(get())
+    }
 }
