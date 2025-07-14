e9fa07b491a0815936eb4a0704c8be4fcead92be
IgnatovaNatalya <ananjeva@gmail.com>
2025-07-14 16:39:17 +0300

    Добавляем в зависимости  SearchVacanciesInteractor и MainViewModel

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -1,10 +1,12 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.core.module.dsl.viewModel
+import ru.practicum.android.diploma.presentation.main.MainViewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.presentation.vacancy.VacancyDetailsViewModel
 
 val viewModelModule = module {
+    viewModel<MainViewModel> { MainViewModel(get()) }
     viewModel {
         VacancyDetailsViewModel(get(), get())
     }
