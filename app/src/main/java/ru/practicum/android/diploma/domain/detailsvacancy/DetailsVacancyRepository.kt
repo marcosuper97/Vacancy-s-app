fff7ee25cc6724f976cdc2622ac335c615e0ad59
marcosuper97 <gorpishin97@yandex.ru>
2025-07-15 21:37:39 +0300

    Фикс репозитория [4]

--- app/src/main/java/ru/practicum/android/diploma/domain/detailsvacancy/DetailsVacancyRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/detailsvacancy/DetailsVacancyRepository.kt
@@ -1,7 +1,8 @@
 package ru.practicum.android.diploma.domain.detailsvacancy
 
+import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.domain.models.VacancyDetails
 
 interface DetailsVacancyRepository {
-    suspend fun doRequest(vacancyId: String): VacancyDetails
+    suspend fun doRequest(vacancyId: String): Result<VacancyDetails>
 }
