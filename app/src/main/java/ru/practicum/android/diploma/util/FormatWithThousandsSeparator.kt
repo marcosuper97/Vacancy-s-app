38271e86940d7364e53ef4aa87aaaffb0652e306
marcosuper97 <gorpishin97@yandex.ru>
2025-07-15 23:44:40 +0300

    Детект

--- app/src/main/java/ru/practicum/android/diploma/util/FormatWithThousandsSeparator.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/FormatWithThousandsSeparator.kt
@@ -3,5 +3,8 @@ package ru.practicum.android.diploma.util
 import java.util.Locale
 
 fun Int?.formatWithThousandsSeparator(): String {
-    return this.let { String.format(Locale.US,"%,d", it).replace(',', ' ') } ?: ""
+    return this.let {
+        String.format(Locale.US, "%,d", it)
+            .replace(',', ' ')
+    }
 }
