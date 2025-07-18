da6cab9e9f97ea08faa3f290d1117b3ee1df33f4
marcosuper97 <gorpishin97@yandex.ru>
2025-07-18 15:47:20 +0300

    Интеграция запросов регионов и отраслей

--- app/src/main/java/ru/practicum/android/diploma/data/network/NetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/NetworkClient.kt
@@ -1,9 +1,13 @@
 package ru.practicum.android.diploma.data.network
 
+import ru.practicum.android.diploma.data.dto.area.AreaDto
+import ru.practicum.android.diploma.data.dto.industry.IndustryGroupDto
 import ru.practicum.android.diploma.data.dto.vacancy.vacancydetails.VacancyDetailsDto
 import ru.practicum.android.diploma.data.dto.vacancy.vacanysearch.VacancySearchResponseDto
 
 interface NetworkClient {
     suspend fun detailsVacancyRequest(requestId: String): Result<VacancyDetailsDto>
     suspend fun vacanciesSearchRequest(requestQuery: Map<String, String>): Result<VacancySearchResponseDto>
+    suspend fun getAreas(): Result<List<AreaDto>>
+    suspend fun getIndustries(): Result<List<IndustryGroupDto>>
 }
