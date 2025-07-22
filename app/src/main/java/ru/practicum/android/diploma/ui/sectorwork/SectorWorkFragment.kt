fa245f06aea2a6b1c192a8856df46cd3c6d5d091
marcosuper97 <gorpishin97@yandex.ru>
2025-07-22 15:54:35 +0300

    промежуточная проработка фрагмента отраслей

--- app/src/main/java/ru/practicum/android/diploma/ui/sectorwork/SectorWorkFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/sectorwork/SectorWorkFragment.kt
@@ -1,20 +1,34 @@
 package ru.practicum.android.diploma.ui.sectorwork
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentSectorWorkBinding
+import ru.practicum.android.diploma.domain.models.Industry
 import ru.practicum.android.diploma.presentation.sectorwork.SectorWorkViewModel
+import ru.practicum.android.diploma.ui.country.AreasAdapter
+import ru.practicum.android.diploma.ui.region.RegionFragment
+import ru.practicum.android.diploma.ui.region.RegionFragment.Companion
+import ru.practicum.android.diploma.ui.region.RegionState
 
 class SectorWorkFragment : Fragment() {
 
     private var _binding: FragmentSectorWorkBinding? = null
     private val binding: FragmentSectorWorkBinding get() = _binding!!
+    private var adapter: AreasAdapter? = null
     private val viewModel: SectorWorkViewModel by viewModel()
 
     override fun onCreateView(
@@ -26,16 +40,134 @@ class SectorWorkFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        adapter = AreasAdapter(::onItemClick)
+        binding.industryUnput.setOnTouchListener(::handleTouchEvent)
+        binding.industriesRev.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        binding.industriesRev.adapter = adapter
+        binding.industryUnput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    binding.industryUnput.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                        0,
+                        0,
+                        R.drawable.ic_search_24,
+                        0
+                    )
+                    viewModel.searchIndustry(CLEAN_REQUEST)
+                } else {
+                    binding.industryUnput.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                        0,
+                        0,
+                        R.drawable.cross,
+                        0
+                    )
+                    viewModel.searchIndustry(s.toString())
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        })
+
+        lifecycleScope.launch {
+            viewModel.state.collect { state ->
+                renderUi(state)
+            }
+        }
 
         binding.toolbarSectorWork.setNavigationOnClickListener {
             findNavController().navigateUp()
         }
     }
 
+    private fun renderUi(state: IndustryState) {
+        Log.d("fragment", state.toString())
+        when (state) {
+            is IndustryState.Content -> {
+                adapter?.update(state.areas)
+                hidePlaceholder()
+                binding.progressBar.visibility = View.GONE
+                binding.industriesRev.visibility = View.VISIBLE
+            }
+
+            IndustryState.Error -> {
+                binding.progressBar.visibility = View.GONE
+                binding.industriesRev.visibility = View.GONE
+                showPlaceholder(ERROR)
+            }
+
+            IndustryState.Loading -> {
+                binding.industriesRev.visibility = View.GONE
+                binding.progressBar.visibility = View.VISIBLE
+                hidePlaceholder()
+            }
+
+            IndustryState.NotFound -> {
+                binding.progressBar.visibility = View.GONE
+                binding.industriesRev.visibility = View.GONE
+                showPlaceholder(NOT_FOUND)
+            }
+
+            is IndustryState.Search -> {
+                hidePlaceholder()
+                adapter?.update(state.areas)
+                binding.progressBar.visibility = View.GONE
+                binding.industriesRev.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun showPlaceholder(boolean: Boolean) {
+        if (boolean) {
+            binding.errorText.text = getString(R.string.couldnt_get_list)
+            binding.industriesPlaceholder.setImageResource(R.drawable.error_carpet_placeholder)
+            binding.errorText.visibility = View.VISIBLE
+            binding.industriesPlaceholder.visibility = View.VISIBLE
+        } else {
+            binding.errorText.text = getString(R.string.region_is_not_found)
+            binding.industriesPlaceholder.setImageResource(R.drawable.error_cat_placeholder)
+            binding.errorText.visibility = View.VISIBLE
+            binding.industriesPlaceholder.visibility = View.VISIBLE
+        }
+    }
+
+    private fun hidePlaceholder() {
+        binding.industriesPlaceholder.visibility = View.GONE
+        binding.errorText.visibility = View.GONE
+    }
+
+    private fun handleTouchEvent(v: View, event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_UP &&
+            event.rawX >= binding.industryUnput.right - binding.industryUnput.compoundPaddingEnd
+        ) {
+            viewModel.searchIndustry(CLEAN_REQUEST)
+            binding.industryUnput.text.clear()
+            return true
+        }
+        return false
+    }
+
+    private fun onItemClick(industry: Industry) {
+        viewModel.industryUpdate(industry)
+        findNavController().navigateUp()
+    }
+
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private const val CLEAN_REQUEST = ""
+        private const val NOT_FOUND = false
+        private const val ERROR = true
     }
 }
