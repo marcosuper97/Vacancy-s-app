ec3db982b51221cfce950fe450d6c8a4a6410f18
marcosuper97 <gorpishin97@yandex.ru>
2025-07-23 05:34:03 +0300

    Верстка плюс большая логика экрана фильтров

--- app/src/main/java/ru/practicum/android/diploma/presentation/filtering/FilteringViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filtering/FilteringViewModel.kt
@@ -2,6 +2,8 @@ package ru.practicum.android.diploma.presentation.filtering
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -9,8 +11,19 @@ import ru.practicum.android.diploma.domain.interactors.FiltersInteractor
 import ru.practicum.android.diploma.domain.models.Filters
 
 class FilteringViewModel(private val interactor: FiltersInteractor) : ViewModel() {
-    private val _screenState = MutableStateFlow<FilteringScreenState>(FilteringScreenState(null))
+    private val defaultFilter = Filters(
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+    )
+    private val _screenState = MutableStateFlow<FilteringScreenState>(FilteringScreenState(defaultFilter))
     val screenState: StateFlow<FilteringScreenState> = _screenState
+    private var searchJob: Job? = null
 
     init {
         viewModelScope.launch {
@@ -22,23 +35,43 @@ class FilteringViewModel(private val interactor: FiltersInteractor) : ViewModel(
         }
     }
 
-    fun clearAreas(){
-
+    fun clearAreas() {
+        viewModelScope.launch {
+            interactor.clearAreas()
+        }
     }
 
-    fun clearIndustry(){
-
+    fun clearIndustry() {
+        viewModelScope.launch {
+            interactor.clearIndustry()
+        }
     }
 
-    fun updateSalary(){
-
+    fun updateNoSalary(onlyWithSalary: Boolean) {
+        viewModelScope.launch {
+            interactor.updateNoSalary(onlyWithSalary)
+        }
     }
 
-    fun updateNoSalary(){
-
+    fun updateSalary(query: String) {
+        searchJob?.cancel()
+        if (query.isEmpty()) {
+            viewModelScope.launch {
+                interactor.updateSalary(null)
+            }
+            return
+        }
+        searchJob = viewModelScope.launch {
+            delay(UPDATE_DELAY)
+            interactor.updateSalary(query)
+        }
     }
 
     suspend fun reset() {
         interactor.reset()
+    }
+
+    companion object {
+        private const val UPDATE_DELAY = 1000L
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/filtering/FilteringFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filtering/FilteringFragment.kt
@@ -1,14 +1,17 @@
 package ru.practicum.android.diploma.ui.filtering
 
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.content.res.ColorStateList
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.core.content.ContextCompat
-import androidx.core.view.isVisible
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -19,7 +22,7 @@ import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFilteringBinding
-import ru.practicum.android.diploma.presentation.filtering.FilteringScreenState
+import ru.practicum.android.diploma.domain.models.Filters
 import ru.practicum.android.diploma.presentation.filtering.FilteringViewModel
 
 class FilteringFragment : Fragment() {
@@ -27,9 +30,7 @@ class FilteringFragment : Fragment() {
     private var _binding: FragmentFilteringBinding? = null
     private val binding: FragmentFilteringBinding get() = _binding!!
     private val viewModel: FilteringViewModel by viewModel()
-
-    private var areaEdit: TextInputLayout? = null
-    private var industryEdit: TextInputLayout? = null
+    private var salaryTextWatcher: TextWatcher? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,136 +44,169 @@ class FilteringFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setupObserver()
+        salaryTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                println(s)
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.isNullOrEmpty()) {
+                    viewModel.updateSalary(s.toString())
+                    if (binding.salaryInput.isFocused) {
+                        binding.salaryHint.setTextColor(ContextCompat.getColor(requireContext(), R.color.blue))
+                    } else {
+                        binding.salaryHint.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+                    }
+                    binding.deleteCross.visibility = View.VISIBLE
+                } else {
+                    binding.deleteCross.visibility = View.GONE
+                    viewModel.updateSalary(CLEAN_REQUEST)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                println(s)
+            }
+        }
+        binding.salaryInput.addTextChangedListener(salaryTextWatcher)
+
+        binding.deleteCross.setOnClickListener {
+            binding.salaryInput.setText(CLEAN_REQUEST)
+            viewModel.updateSalary(CLEAN_REQUEST)
+            if (binding.salaryInput.isFocused) {
+                binding.salaryHint.setTextColor(ContextCompat.getColor(requireContext(), R.color.blue))
+            } else {
+                binding.salaryHint.setTextColor(ContextCompat.getColor(requireContext(), R.color.salary_filed_color))
+            }
+        }
+
+        binding.salaryInput.setOnEditorActionListener { _, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val inputMethodManager =
+                    requireContext().getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+                inputMethodManager?.hideSoftInputFromWindow(binding.salaryInput.windowToken, 0)
+                binding.salaryInput.clearFocus()
+                if (binding.salaryInput.text.isNotEmpty()) {
+                    binding.salaryHint.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+                } else {
+                    binding.salaryHint.setTextColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.salary_filed_color
+                        )
+                    )
+                }
+                true
+            } else {
+                false
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.screenState.collect { state ->
+                    renderUi(state.filters)
+                }
+            }
+        }
+
+        binding.areaNavigation.setOnClickListener {
+            findNavController().navigate(R.id.action_to_place_work_fragment)
+        }
+
+        binding.sectorNavigation.setOnClickListener {
+            findNavController().navigate(R.id.action_to_sector_work_fragment)
+        }
 
         binding.toolbarFiltering.setNavigationOnClickListener {
             findNavController().navigateUp()
         }
 
-        listOf(
-            binding.area,
-            binding.areaEdit,
-        ).forEach {
-            it.setOnClickListener {
-                findNavController().navigate(R.id.action_to_place_work_fragment)
-            }
-        }
-
-        listOf(
-            binding.sector,
-            binding.sectorEdit,
-        ).forEach {
-            it.setOnClickListener {
-                findNavController().navigate(R.id.action_to_place_work_fragment)
-            }
-        }
-
-        binding.area.setEndIconOnClickListener {
-            viewModel.updateArea(null)
-            binding.areaEdit.setText("")
-            areaEdit?.endIconDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.arrow_forward_inset)
-
-        }
-
-        binding.sector.setEndIconOnClickListener {
-            viewModel.updateIndustry(null)
-            binding.sectorEdit.setText("")
-            industryEdit?.endIconDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.arrow_forward_inset)
-
-        }
-
-        binding.salaryInput.setEndIconOnClickListener {
-            viewModel.updateSalary(null)
-            binding.salaryEdit.setText("")
-        }
-
-        binding.salaryEdit.setOnFocusChangeListener { _, hasFocus ->
-            if (hasFocus && binding.salaryEdit.text.toString() == getString(R.string.enter_the_amount)) {
-                binding.salaryEdit.text?.clear()
-            }
-        }
-
-        binding.salaryCheckBox.setOnCheckedChangeListener { _, isChecked ->
-            viewModel.updateOnlyWithSalary(isChecked)
-        }
-
-        binding.salaryEdit.addTextChangedListener {
-            val text = binding.salaryEdit.text?.toString()
-            val salary = text?.toIntOrNull()
-            viewModel.updateSalary(salary)
-        }
 
         binding.apply.setOnClickListener {
             findNavController().navigateUp()
         }
 
         binding.drop.setOnClickListener {
-            reset()
-        }
-    }
-
-    private fun setupObserver() {
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.screenState.collect { state ->
-                    render(state)
-                }
+            lifecycleScope.launch {
+                viewModel.reset()
             }
         }
     }
 
-    private fun render(state: FilteringScreenState) {
-        if (state.thereIsFilters) {
-            showFilters(state)
+    private fun renderUi(state: Filters) {
+        if (!state.salary.isNullOrEmpty()) {
+            binding.salaryInput.setText(state.salary)
+        }
+        showButtons(state)
+        checkBoxState(state.onlyWithSalary)
+        areaFieldBehavior(
+            binding.area,
+            binding.areaEdit,
+            R.id.action_to_place_work_fragment,
+            listOf(state.country, state.area),
+        )
+        industryFieldBehavior(
+            binding.sector,
+            binding.sectorEdit,
+            R.id.action_to_sector_work_fragment,
+            state.industry
+        )
+    }
+
+    private fun Filters.hasAnyNonNull(): Boolean {
+        return country != null ||
+            countryId != null ||
+            area != null ||
+            areaId != null ||
+            industry != null ||
+            industryId != null ||
+            salary != null ||
+            onlyWithSalary != null
+    }
+
+    private fun showButtons(filters: Filters) {
+        if (filters.hasAnyNonNull()) {
+            binding.drop.visibility = View.VISIBLE
+            binding.apply.visibility = View.VISIBLE
+        } else {
+            binding.drop.visibility = View.GONE
+            binding.apply.visibility = View.GONE
         }
     }
 
-    private fun showFilters(state: FilteringScreenState) {
-        if (state.filters?.area != null) {
-            binding.areaEdit.setText(state.filters.area)
-            areaEdit = binding.area
-            areaEdit!!.endIconDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.cross)
-        }
-        if (state.filters?.industry != null) {
-            binding.sectorEdit.setText(state.filters.industry)
-            industryEdit = binding.sector
-            industryEdit!!.endIconDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.cross)
-        }
-        if (state.filters?.salary != null) {
-            binding.salaryEdit.apply {
-                setText(state.filters.salary.toString())
-                setTextColor(ContextCompat.getColor(requireContext(), R.color.text_color))
-            }
-        }
-        binding.salaryCheckBox.isChecked = (state.filters?.onlyWithSalary == true)
-
-        binding.apply.isVisible = true
-        binding.drop.isVisible = true
+    private fun checkBoxState(state: Boolean?) {
+        if (state == null) {
+            binding.checkbox.isEnabled = false
+        } else binding.checkbox.isEnabled = state
     }
 
-    private fun textFieldBehavior(
+    private fun areaFieldBehavior(
         textInputLayout: TextInputLayout,
         editText: EditText,
         navigationActionId: Int,
-        text: String?
+        textAreas: List<String?>?
     ) {
-        if (!text.isNullOrEmpty()) {
-            textInputLayout.setEndIconDrawable(R.drawable.cross)
-            editText.setText(text)
-            textInputLayout.defaultHintTextColor = ColorStateList.valueOf(requireContext().getColor(R.color.text_color))
+        textInputLayout.setEndIconDrawable(R.drawable.cross)
+        val areasText: String = with(StringBuilder()) {
+            textAreas?.get(0)?.let { append(it) }
+            textAreas?.get(1)?.let { append(", $it") }
+            toString()
+        }
+        if (areasText.isNotEmpty()) {
+            editText.setText(areasText)
+            textInputLayout.defaultHintTextColor =
+                ColorStateList.valueOf(requireContext().getColor(R.color.text_color))
         } else {
             textInputLayout.setEndIconDrawable(R.drawable.arrow_forward_24)
             textInputLayout.defaultHintTextColor = ColorStateList.valueOf(requireContext().getColor(R.color.grey))
             editText.text.clear()
         }
+
         textInputLayout.setEndIconOnClickListener {
-            if (!text.isNullOrEmpty()) {
+            if (!areasText.isNullOrEmpty()) {
                 editText.text.clear()
-                if (navigationActionId == R.id.action_to_country_fragment) {
-                    viewModel.deleteCountry()
-                } else {
-                    viewModel.deleteRegion()
-                }
+                viewModel.clearAreas()
                 textInputLayout.setEndIconDrawable(R.drawable.arrow_forward_24)
             } else {
                 findNavController().navigate(navigationActionId)
@@ -180,22 +214,36 @@ class FilteringFragment : Fragment() {
         }
     }
 
-    private fun reset() {
+    private fun industryFieldBehavior(
+        textInputLayout: TextInputLayout,
+        editText: EditText,
+        navigationActionId: Int,
+        textIndustry: String?
+    ) {
+        if (!textIndustry.isNullOrEmpty()) {
+            textInputLayout.setEndIconDrawable(R.drawable.cross)
+            editText.setText(textIndustry)
+            textInputLayout.defaultHintTextColor =
+                ColorStateList.valueOf(requireContext().getColor(R.color.text_color))
+        } else {
+            textInputLayout.setEndIconDrawable(R.drawable.arrow_forward_24)
+            textInputLayout.defaultHintTextColor = ColorStateList.valueOf(requireContext().getColor(R.color.grey))
+            editText.text.clear()
+        }
 
-        binding.apply.isVisible = false
-        binding.drop.isVisible = false
-        viewModel.updateArea(null)
-        binding.areaEdit.setText("")
-        viewModel.updateIndustry(null)
-        binding.sectorEdit.setText("")
-        viewModel.clearSalary()
-        binding.salaryEdit.setText("")
-        viewModel.updateOnlyWithSalary(null)
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.reset()
+        textInputLayout.setEndIconOnClickListener {
+            if (!textIndustry.isNullOrEmpty()) {
+                editText.text.clear()
+                viewModel.clearIndustry()
+                textInputLayout.setEndIconDrawable(R.drawable.arrow_forward_24)
+            } else {
+                findNavController().navigate(navigationActionId)
             }
         }
+    }
+
+    companion object {
+        private const val CLEAN_REQUEST = ""
     }
 }
 
