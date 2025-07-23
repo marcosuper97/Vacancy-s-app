680a31a3dbea0171f7074b8b38173ebc427d7cd7
marcosuper97 <gorpishin97@yandex.ru>
2025-07-23 06:54:07 +0300

    Мини баги фикс

--- app/src/main/java/ru/practicum/android/diploma/ui/main/MainFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/main/MainFragment.kt
@@ -37,7 +37,7 @@ class MainFragment : Fragment() {
     private val viewModel: MainViewModel by viewModel()
     private var vacanciesAdapter: VacanciesAdapter? = null
     private var menuItem: MenuItem? = null
-    private var requestText: String = ""
+    private var requestText: String = EMPTY_REQUEST
 
 
     override fun onCreateView(
@@ -70,7 +70,7 @@ class MainFragment : Fragment() {
 
         findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<Boolean>("reapplyFilters")
             ?.observe(viewLifecycleOwner) { reapply ->
-                if (reapply) {
+                if (reapply && requestText.isNotEmpty()) {
                     viewModel.reapplyRequest(requestText)
                     findNavController().currentBackStackEntry?.savedStateHandle?.set("reapplyFilters", false)
                 }
@@ -244,6 +244,7 @@ class MainFragment : Fragment() {
         ) {
             viewModel.onClearSearchClicked()
             binding.mainInputEt.text.clear()
+            requestText = EMPTY_REQUEST
             return true
         }
         return false
@@ -254,5 +255,9 @@ class MainFragment : Fragment() {
             false -> menuItem?.setIcon(R.drawable.filters)
             true -> menuItem?.setIcon(R.drawable.filters_active)
         }
+    }
+
+    companion object {
+        private const val EMPTY_REQUEST = ""
     }
 }
